ba40e601b725ad2f011bb35b8f09e3e8aaa33bd0
ggnohc <gheegnohc@gmail.com>
2018-12-10 22:40:38 +0800

    Added pick_objects.sh

---
+++ src/ShellScripts/pick_objects.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+xterm -e " cd /home/workspace/catkin_ws; catkin_make; source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/U-world/U-world.world" &
+sleep 5
+xterm -e " cd /home/workspace/catkin_ws; source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch  map_file:=/home/workspace/catkin_ws/src/World/U-world/U-World-Map.yaml" &
+sleep 5
+xterm -e " cd /home/workspace/catkin_ws; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm -e " cd /home/workspace/catkin_ws; source devel/setup.bash; rosrun pick_objects pick_objects" &
