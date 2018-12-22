5acc1208b7e0340106d56da6b8230687a8d4f26b
ggnohc <gheegnohc@gmail.com>
2018-12-23 00:02:45 +0800

    Updated test_slam.sh and wall_follower.sh

--- src/ShellScripts/test_slam.sh
+++ src/ShellScripts/test_slam.sh
@@ -1,15 +1,22 @@
-#!/bin/sh
-#xterm  -e  " cd /home/workspace/catkin_ws; catkin_make; source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch  " &
-#sleep 5
-#xterm  -e  " cd /home/workspace/catkin_ws; catkin_make; source devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch " &
-#sleep 5
-#xterm  -e  " cd /home/workspace/catkin_ws; catkin_make; source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch  " &
-#sleep 5
+#!/bin/bash
 
-xterm -e " cd /home/workspace/catkin_ws; catkin_make; source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/U-world/U-world.world" &
+# Build the catkin_ws
+#cd $(pwd)/../..; catkin_make
+
+# Launch the nodes
+#xterm  -e "source devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/world/home_service_robot.world"; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+#sleep 10
+#xterm  -e "source devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
+#sleep 5
+#xterm  -e "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+#sleep 5
+#xterm  -e "source devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
+
+xterm -e "cd /home/workspace/catkin_ws; catkin_make; source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/U-world/U-world.world" &
+sleep 15
+#xterm -e "rosrun gmapping slam_gmapping" &
+xterm -e "cd /home/workspace/catkin_ws; source devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
-xterm -e "rosrun gmapping slam_gmapping" &
+xterm -e "cd /home/workspace/catkin_ws; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-xterm -e " cd /home/workspace/catkin_ws; catkin_make; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
-sleep 5
-xterm -e " cd /home/workspace/catkin_ws; catkin_make; source devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch" &
+xterm -e "cd /home/workspace/catkin_ws; source devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch" &
--- src/ShellScripts/wall_follower.sh
+++ src/ShellScripts/wall_follower.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
+
+#xterm  -e "source devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/world/home_service_robot.world"; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+#sleep 10
+#xterm  -e "source devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch custom_gmapping_launch_file:="$(pwd)/src/slam_gmapping/gmapping/launch/slam_gmapping_pr2.launch"  " &
+#sleep 5
+#xterm  -e "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+#sleep 5
+#xterm  -e "source devel/setup.bash; rosrun wall_follower wall_follower"
+
+
 xterm -e " cd /home/workspace/catkin_ws; catkin_make; source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/U-world/U-world.world" &
-sleep 5
-xterm -e "roslaunch gmapping.launch" &   #where do i get the gmapping.launch
+sleep 10 
+#xterm -e "roslaunch gmapping.launch" &   #where do i get the gmapping.launch
+xterm  -e "cd /home/workspace/catkin_ws; source devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch custom_gmapping_launch_file:="/home/workspace/catkin_ws/src/slam_gmapping/gmapping/launch/slam_gmapping_pr2.launch"  " &
 sleep 5
 xterm -e " cd /home/workspace/catkin_ws; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
