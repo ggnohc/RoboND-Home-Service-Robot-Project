d9c34c2d4c6d13fb82dc9c91b5027ff1f412310a
ggnohc <gheegnohc@gmail.com>
2018-12-23 00:29:05 +0800

    Updated pick-up and drop-off according to U-world

--- src/pick_objects/src/pick_objects.cpp
+++ src/pick_objects/src/pick_objects.cpp
@@ -1,13 +1,15 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <tf/tf.h>
+
 
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 int main(int argc, char** argv){
-  // Initialize the simple_navigation_goals node
-    ros::init(argc, argv, "pick_objects");
+  // Initialize the pick_objects node
+  ros::init(argc, argv, "pick_objects");
 
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
@@ -17,54 +19,38 @@ int main(int argc, char** argv){
     ROS_INFO("Waiting for the move_base action server to come up");
   }
 
-    move_base_msgs::MoveBaseGoal pickupGoal;
+  move_base_msgs::MoveBaseGoal goal;
 
   // set up the frame parameters
-    pickupGoal.target_pose.header.frame_id = "map";
-    pickupGoal.target_pose.header.stamp = ros::Time::now();
+  goal.target_pose.header.frame_id = "map";
+  goal.target_pose.header.stamp = ros::Time::now();
 
   // Define a position and orientation for the robot to reach
-    pickupGoal.target_pose.pose.position.x = 4.0;
-    pickupGoal.target_pose.pose.position.y = 0.0;
-    pickupGoal.target_pose.pose.orientation.w = 1.0;
 
-    // Send the pickupGoal position and orientation for the robot to reach
-    ROS_INFO("Sending pickup location");
-    ac.sendGoal(pickupGoal);
+  float goals[2][3] = { {4.0, 0.0, 1.0}, {-4.0, 7.0, 1.0}  };
 
-  // Wait an infinite time for the results
-  ac.waitForResult();
+  int num_points = 2;
 
-  // Check if the robot reached its goal
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-        ROS_INFO("Arrived at pickup location");
-  else
-        ROS_INFO("Orientation deficit - failed to find pickup location");
+  for (int i=0; i < num_points; i++){
 
+      goal.target_pose.pose.position.x = goals[i][0];
+      goal.target_pose.pose.position.y = goals[i][1];
+      goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(goals[i][2]);
 
-    move_base_msgs::MoveBaseGoal dropoffGoal;
+       // Send the goal position and orientation for the robot to reach
+      ROS_INFO("Sending goal");
+      ac.sendGoal(goal);
 
-    // set up the frame parameters
-    dropoffGoal.target_pose.header.frame_id = "map";
-    dropoffGoal.target_pose.header.stamp = ros::Time::now();
+      // Wait an infinite time for the results
+      ac.waitForResult();
 
-    // Define a position and orientation for the robot to reach
-    dropoffGoal.target_pose.pose.position.x = -4.0;
-    dropoffGoal.target_pose.pose.position.y = 7.0;
-    dropoffGoal.target_pose.pose.orientation.w = 1.0;
-
-    // Send the dropoffGoal position and orientation for the robot to reach
-    ROS_INFO("Sending dropoff location");
-    ac.sendGoal(dropoffGoal);
-
-    // Wait an infinite time for the results
-    ac.waitForResult();
-
-    // Check if the robot reached its goal
-    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-        ROS_INFO("Arrived at dropoff location");
-    else
-        ROS_INFO("Orientation deficit - failed to find dropoff location");
+      // Check if the robot reached its goal
+      if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+        ROS_INFO("Goal reached!");
+      else
+        ROS_INFO("Failed to reach goal for some reason");
+      ros::Duration(5.0).sleep();
+  }
 
   return 0;
 }
